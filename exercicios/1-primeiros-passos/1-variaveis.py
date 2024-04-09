671c660578b4328914d63612789ae75bda0fe77a
Kleber Alamar <kleberwsantos@gmail.com>
2024-04-09 23:40:43 +0000

    resolucao exercico variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Kleber alamar'
 
 # Declare variáveis de cada tipo numérico
-
+idade = 43
 
 # Declare uma variável do tipo booleano
+estudante = True
